60e56233fd683a5f03a6cf7d36d95096e4034366
Jon Evans <jon.evans@ixxus.com>
2015-07-14 18:26:54 +0100

    Change install params for Amazon

--- manifests/params.pp
+++ manifests/params.pp
@@ -99,14 +99,14 @@ class nodejs::params {
         $repo_class                = '::nodejs::repo::nodesource'
       }
       elsif ($::operatingsystem == 'Amazon') {
-        $manage_package_repo       = true
+        $manage_package_repo       = false
         $nodejs_debug_package_name = 'nodejs-debuginfo'
         $nodejs_dev_package_name   = 'nodejs-devel'
         $nodejs_package_name       = 'nodejs'
-        $npm_package_ensure        = 'absent'
+        $npm_package_ensure        = 'present'
         $npm_package_name          = 'npm'
         $npm_path                  = '/usr/bin/npm'
-        $repo_class                = '::nodejs::repo::nodesource'
+        $repo_class                = undef
       }
       else {
         fail("The ${module_name} module is not supported on ${::operatingsystem} ${::operatingsystemrelease}.")
